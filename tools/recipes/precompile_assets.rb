972e44ebaae35ada60d23a0e78a8c0789f21e229
Simon Bettison <simon@bettison.org>
2014-01-25 12:12:01 +0000

    modified precompile recipe

---
+++ tools/recipes/precompile_assets.rb
@@ -0,0 +1,15 @@
+#thanks to DAVID EISINGER @ VIGET
+#http://viget.com/extend/aws-opsworks-lessons-learned
+
+rails_env = new_resource.environment["RAILS_ENV"]
+Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
+
+Chef::Log.info("ENV: #{node[:custom_env][:portfoliobuilder].merge({RAILS_ENV: rails_env}).inspect}")
+
+bash "rake assets:precompile" do
+  cwd release_path  
+  command "bundle exec rake assets:precompile"
+  environment node[:custom_env][:portfoliobuilder].merge({RAILS_ENV: rails_env})
+end
+  
+  
