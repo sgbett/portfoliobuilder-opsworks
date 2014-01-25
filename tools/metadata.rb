b69cc7d91c050eaf2132bc2fb6b1c35400b058b8
Simon Bettison <simon@bettison.org>
2014-01-25 04:13:23 +0000

    updated version to be compatiable with opsworks

--- tools/metadata.rb
+++ tools/metadata.rb
@@ -5,21 +5,6 @@ maintainer_email "sgbett@remailer.org"
 license          "GPL"
 description      "misc recipes for portfoliobuilder"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
-version          "0.1"
-
-# recommends 'build-essential'
-# recommends 'xml'
-# recommends 'java'
-# recommends 'monit'
-# 
-# provides 'elasticsearch'
-# provides 'elasticsearch::data'
-# provides 'elasticsearch::ebs'
-# provides 'elasticsearch::aws'
-# provides 'elasticsearch::nginx'
-# provides 'elasticsearch::proxy'
-# provides 'elasticsearch::plugins'
-# provides 'elasticsearch::monit'
-# provides 'elasticsearch::search_discovery'
+version          "0.0.1"
 
 provides 'tools::precompile_assets'
