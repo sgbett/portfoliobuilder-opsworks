5d3910af26cbbb02ebce684d5a8e20077a7a2509
Simon Bettison <simon@bettison.org>
2014-01-27 21:24:02 +0000

    user service restart instead of executing via shell

--- sidekiq/recipes/restart.rb
+++ sidekiq/recipes/restart.rb
@@ -5,21 +5,25 @@
 
 include_recipe "deploy"
 include_recipe "sidekiq"
-
+  
 node[:deploy].each do |application, deploy|
   if deploy[:application_type] != 'rails'
     Chef::Log.debug("Skipping sidekiq::restart application #{application} as it is not a Rails app")
     next
   end
-  
-  execute "restart sidekiq" do
-    cwd deploy[:current_path]
-    command "sidekiq restart"
-    action :run
-    
-    only_if do 
-      File.exists?(deploy[:current_path])
-    end
+
+  service "sidekiq" do
+    action :restart
   end
+  
+#   execute "restart sidekiq" do
+#     cwd deploy[:current_path]
+#     command "sidekiq restart"
+#     action :run
+#     
+#     only_if do 
+#       File.exists?(deploy[:current_path])
+#     end
+#   end
     
 end
