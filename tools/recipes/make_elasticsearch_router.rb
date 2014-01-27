b872c0bcaeae7d1584f906d1517569ed11c59c6c
Simon Bettison <simon@bettison.org>
2014-01-27 06:19:51 +0000

    xgdgf

--- tools/recipes/make_elasticsearch_router.rb
+++ tools/recipes/make_elasticsearch_router.rb
@@ -1,9 +1,9 @@
 # Create ES config file
 #
 template "elasticsearch.yml" do
-  path   "#{node.elasticsearch[:path][:conf]}/elasticsearch.yml"
+  path   "/usr/local/elasticsearch.yml"
   source "elasticsearch.yml.erb"
-  owner node.elasticsearch[:user] and group node.elasticsearch[:user] and mode 0755
+  owner 'elasticsearch' and group 'elasticsearch' and mode 0755
 
-  notifies :restart, 'service[elasticsearch]' unless node.elasticsearch[:skip_restart]
+  notifies :restart, 'service[elasticsearch]' 
 end
