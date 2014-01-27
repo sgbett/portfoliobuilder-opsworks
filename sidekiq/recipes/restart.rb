421233f9a79db27eeef2a4277865039fa8005290
Simon Bettison <simon@bettison.org>
2014-01-27 19:26:13 +0000

    restart

--- sidekiq/recipes/restart.rb
+++ sidekiq/recipes/restart.rb
@@ -1,19 +1,19 @@
 #
 # Cookbook Name:: sidekiq
-# Recipe:: start
+# Recipe:: restart
 #
 
 include_recipe "deploy"
 
 node[:deploy].each do |application, deploy|
   if deploy[:application_type] != 'rails'
-    Chef::Log.debug("Skipping sidekiq::start application #{application} as it is not a Rails app")
+    Chef::Log.debug("Skipping sidekiq::restart application #{application} as it is not a Rails app")
     next
   end
   
-  execute "start sidekiq" do
+  execute "restart sidekiq" do
     cwd deploy[:current_path]
-    command "sidekiq start"
+    command "sidekiq restart"
     action :run
     
     only_if do 
