7986c34ccd9d40bfcac830deefa7166958affc1f
Simon Bettison <simon@bettison.org>
2014-01-27 19:38:11 +0000

    sdsdf

--- sidekiq/metadata.rb
+++ sidekiq/metadata.rb
@@ -4,10 +4,10 @@ maintainer       "sgbett"
 maintainer_email "sgbett@remailer.org"
 license          "GPL"
 description      "opsworks service wrapper for sidekiq"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 provides 'sidekiq'
 provides 'sidekiq::start'
-provides 'sidekiq::stop'
+#provides 'sidekiq::stop'
 provides 'sidekiq::restart'
