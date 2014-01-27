4440a6c5ccee9ea7a010bf2c249a7c6a4f1c8ffa
Simon Bettison <simon@bettison.org>
2014-01-27 13:13:52 +0000

    0.0.2

--- tools/metadata.rb
+++ tools/metadata.rb
@@ -5,6 +5,9 @@ maintainer_email "sgbett@remailer.org"
 license          "GPL"
 description      "misc recipes for portfoliobuilder"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
-version          "0.0.1"
+version          "0.0.2"
+
+depends 'elasticsearch'
 
 provides 'tools::precompile_assets'
+provides 'tools::setup_elasticsearch_nondata'
