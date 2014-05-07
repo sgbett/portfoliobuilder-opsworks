8d01ac0954db63f89d4fc98fba6654adf67c8d04
Simon Bettison <simon@bettison.org>
2014-05-07 23:42:52 +0100

    reduce memory for non data java

--- tools/recipes/setup_elasticsearch_nondata.rb
+++ tools/recipes/setup_elasticsearch_nondata.rb
@@ -1,11 +1,23 @@
 include_recipe 'elasticsearch::default'
 include_recipe 'elasticsearch::aws'
 
+node.normal[:elasticsearch][:allocated_memory] = "1600m"
+
 # re-reate ES config file with our 'router' template
 #
 template "elasticsearch.yml" do
   path   "#{node.elasticsearch[:path][:conf]}/elasticsearch.yml"
   source "elasticsearch.yml.erb"
+  owner node.elasticsearch[:user] and group node.elasticsearch[:user] and mode 0755
+
+  notifies :restart, 'service[elasticsearch]' unless node.elasticsearch[:skip_restart]
+end
+
+# recreate file with our newly set 'allocated_memory'
+#
+template "elasticsearch-env.sh" do
+  path   "#{node.elasticsearch[:path][:conf]}/elasticsearch-env.sh"
+  source "elasticsearch-env.sh.erb"
   owner node.elasticsearch[:user] and group node.elasticsearch[:user] and mode 0755
 
   notifies :restart, 'service[elasticsearch]' unless node.elasticsearch[:skip_restart]
