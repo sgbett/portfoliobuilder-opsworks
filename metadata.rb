146c584def8d48bc502850808d37c235ef11f3ae
Simon Bettison <simon@bettison.org>
2014-08-03 15:09:33 +0100

    consistency

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer       "sgbett"
 maintainer_email "sgbett@remailer.org"
 license          "GPL"
 description      "custom cookbook for portfoliobuilder opsworks stack"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
 version          "0.0.1"
 
 provides 'bigdesk'
