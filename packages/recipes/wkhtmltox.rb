08929efea0d02320b1391ed81b3dd8802d1a978f
Simon Bettison <simon@bettison.org>
2014-09-25 00:30:10 +0100

    typo

--- packages/recipes/wkhtmltox.rb
+++ packages/recipes/wkhtmltox.rb
@@ -1,5 +1,5 @@
 remote_file "/tmp/wkhtmltox-0.12.1_linux-precise-amd64.deb" do
-  source "wget http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.1/wkhtmltox-0.12.1_linux-precise-amd64.deb"
+  source "http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.1/wkhtmltox-0.12.1_linux-precise-amd64.deb"
   mode 0644
   checksum "242ace909f22c4f8e4f58a59cc4319b96ea47ac3440b27c2a58081be8b840eb1"
 end
