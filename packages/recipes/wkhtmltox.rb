31d29bfbefccaaadb991486677eda2e4773965f2
Simon Bettison <simon@bettison.org>
2015-01-26 13:12:31 +0100

    version bump for wkhtmltox

--- packages/recipes/wkhtmltox.rb
+++ packages/recipes/wkhtmltox.rb
@@ -1,10 +1,12 @@
-remote_file "/tmp/wkhtmltox-0.12.1_linux-precise-amd64.deb" do
-  source "http://kent.dl.sourceforge.net/project/wkhtmltopdf/0.12.1/wkhtmltox-0.12.1_linux-precise-amd64.deb"
+VERSION='0.12.2.1'
+
+remote_file "/tmp/wkhtmltox-#{VERSION}_linux-precise-amd64.deb" do
+  source "http://kent.dl.sourceforge.net/project/wkhtmltopdf/#{VERSION}/wkhtmltox-#{VERSION}_linux-precise-amd64.deb"
   mode 0644
-  checksum "242ace909f22c4f8e4f58a59cc4319b96ea47ac3440b27c2a58081be8b840eb1"
+#  checksum "242ace909f22c4f8e4f58a59cc4319b96ea47ac3440b27c2a58081be8b840eb1"
 end
 
 dpkg_package "wkhtmltox" do
-  source "/tmp/wkhtmltox-0.12.1_linux-precise-amd64.deb"
+  source "/tmp/wkhtmltox-#{VERSION}_linux-precise-amd64.deb"
   action :install
 end
