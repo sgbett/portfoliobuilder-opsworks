8d91eb8d919d204426a744e6e29a57133bef0a85
Simon Bettison <simon@bettison.org>
2015-01-26 13:49:46 +0100

    changed mirror

--- packages/recipes/wkhtmltox.rb
+++ packages/recipes/wkhtmltox.rb
@@ -1,7 +1,8 @@
+PROJECT_MIRROR='http://www.mirrorservice.org/sites/download.sourceforge.net/pub/sourceforge/w/wk/wkhtmltopdf'
 VERSION='0.12.2.1'
 
 remote_file "/tmp/wkhtmltox-#{VERSION}_linux-precise-amd64.deb" do
-  source "http://kent.dl.sourceforge.net/project/wkhtmltopdf/#{VERSION}/wkhtmltox-#{VERSION}_linux-precise-amd64.deb"
+  source "#{MIRROR}/#{VERSION}/wkhtmltox-#{VERSION}_linux-precise-amd64.deb"
   mode 0644
 #  checksum "242ace909f22c4f8e4f58a59cc4319b96ea47ac3440b27c2a58081be8b840eb1"
 end
@@ -10,3 +11,4 @@ dpkg_package "wkhtmltox" do
   source "/tmp/wkhtmltox-#{VERSION}_linux-precise-amd64.deb"
   action :install
 end
+
