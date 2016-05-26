96a067b8259303df7c50459eef29a85d58389123
Simon Bettison <simon@bettison.org>
2016-05-26 12:26:03 +0100

    sidekiq v0.0.4

--- sidekiq/metadata.rb
+++ sidekiq/metadata.rb
@@ -5,7 +5,7 @@ maintainer_email "sgbett@remailer.org"
 license          "GPL"
 description      "opsworks service wrapper for sidekiq"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
-version          "0.0.3"
+version          "0.0.4"
 
 provides 'sidekiq'
 provides 'sidekiq::start'
