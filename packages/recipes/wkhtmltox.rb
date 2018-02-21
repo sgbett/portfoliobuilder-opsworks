27eb61462b35533ffd93418bd27f8d4c6c2c5cb7
Simon Bettison <simon@bettison.org>
2018-02-21 17:55:32 +0000

    update wkhtmltox

--- packages/recipes/wkhtmltox.rb
+++ packages/recipes/wkhtmltox.rb
@@ -1,15 +1,19 @@
 MIRROR='https://s3-eu-west-1.amazonaws.com'
 LOCATION='portfoliobuilder-eu'
-VERSION='0.12.2.1'
+VERSION='0.12.4'
 
-remote_file "/tmp/wkhtmltox-#{VERSION}_linux-precise-amd64.deb" do
-  source "#{MIRROR}/#{LOCATION}/wkhtmltox-#{VERSION}_linux-precise-amd64.deb"
+FILENAME="wkhtmltox-#{VERSION}_linux-generic-amd64.tar.xz"
+
+remote_file "/tmp/#{FILENAME}" do
+  source "#{MIRROR}/#{LOCATION}/#{FILENAME}"
   mode 0644
 #  checksum "242ace909f22c4f8e4f58a59cc4319b96ea47ac3440b27c2a58081be8b840eb1"
 end
 
-dpkg_package "wkhtmltox" do
-  source "/tmp/wkhtmltox-#{VERSION}_linux-precise-amd64.deb"
-  action :install
+bash "install-wkhtmltox" do
+  cwd "/tmp"
+  code <<-EOF
+    tar -xvf #{FILENAME}
+    cp -av wkhtmltox/* /usr/local/
+  EOF
 end
-
